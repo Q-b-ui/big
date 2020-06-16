940cc5f99200cb2af3e5892a6ad0c391277a2b44
qian <1021913454@qq.com>
2020-06-16 19:27:12 +0800

    完成登录页

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,87 +1,57 @@
+// 入口函数
 $(function () {
-    // ----------------  切换登录和注册的盒子 -----------------
-    $('.goto-register a').click(function () {
-        $('#register').show().prev().hide();
-    });
-    $('.goto-login a').click(function () {
-        $('#register').hide().prev().show();
-    });
 
-    // ----------------  注册功能  --------------------------
-    // 监听注册表单的提交事件
-    $('#register form').on('submit', function (e) {
+  // 表单验证
+  var form= layui.form;
+  form.verify({
+    username: function(value, item){ //value：表单的值、item：表单的DOM对象
+      if(!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)){
+        return '用户名不能有特殊字符';
+      }
+      if(/(^\_)|(\__)|(\_+$)/.test(value)){
+        return '用户名首尾不能出现下划线\'_\'';
+      }
+      if(/^\d+\d+\d$/.test(value)){
+        return '用户名不能全为数字';
+      }
+    }
+    
+    //我们既支持上述函数式的方式，也支持下述数组的形式
+    //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+    ,pass: [
+      /^[\S]{6,12}$/
+      ,'密码必须6到12位，且不能出现空格'
+    ] 
+  });   
+  // 分析需求：
+  // 1.注册登录按钮点击事件
+  // 2.获取用户输入的信息
+  // 3.发送ajax请求
+  // 4.判断 如果请求成功 跳转首页
+
+  // 1.注册登录按钮点击事件
+  $("#btn-1").on("click", function (e) {
+    // 阻止默认跳转
+  
+    e.preventDefault()
+    // 2.获取用户输入的信息
+    var fd = $("form").serialize();
+    // 3.发送ajax请求
+    $.ajax({
+      type: "post",
+      url: "http://ajax.frontend.itheima.net/api/login",
+      data: fd,
+      success: function (res) {
+        console.log(res);
+    // 4.判断 如果请求成功 跳转首页
+      if(res.status==0){
+        location.href='./index.html'
+      }
         
-        // 阻止默认行为（阻止表单提交的行为）
-        e.preventDefault();
-        // 使用JS收集表单中的数据(获取输入框中的账号和密码)
-        /**
-         * serialize 是根据表单项的name属性值获取值的，所以这里一定要检查表单项的name属性
-         */
-        var data = $(this).serialize();
-        // console.log(data); // username=admin&password=123
-        // 把账号和密码提交给接口，从而完成注册
-        $.ajax({
-            type: 'POST',
-            url: 'http://btapi.ehomespace.com/api/reguser',
-            data: data,
-            success: function (res) {
-                // 无论成功还是失败，都要提示
-                // alert(res.message);
-                layer.msg(res.message);
-                // 并且隐藏注册的盒子，显示登录的盒子
-                if (res.status === 0) {
-                    $('#register').hide().prev().show();
-                }
-            }
-        });
+      
         
+      }
     });
-
-    // ----------------  完成表单验证 ------------------------
-    var form = layui.form;
-    // console.log(form); // 得到一个对象
-    // 调用form.verify() 方法，自定义验证规则
-    form.verify({
-        // 键（验证规则名称）: 值（验证方法，可以使用数组，也可以使用函数）,
-        // len: [正则表达式, 错误提示],
-        len: [/^\w{6,12}$/, '密码长度必须是6到12位'], //{6,12}不要写成{6, 12}
-
-        same: function (value) {
-            // 形参 value 表示使用验证规则的输入的值
-            // return '错误提示';
-            // 获取密码框的值
-            var password = $('#reg-password').val();
-            // 比较 password（密码）和value（确认密码）
-            if (password !== value) {
-                return '两次密码不一致-laotang';
-            }
-        }
-    });
-
-
-    // ------------------ 完成登录功能代码 -------------------
-    // 监听登录表单提交事件
-    $('#login form').on('submit', function (e) {
-
-        // 阻止默认行为
-        e.preventDefault();
-        // 获取账号和密码
-        // ajax提交给接口
-        $.ajax({
-            type: 'POST',
-            url: 'http://btapi.ehomespace.com/api/login',
-            data: $(this).serialize(),
-            success: function (res) {
-                // 无论成功，还是失败，都给出提示
-                // alert(res.message);
-                layer.msg(res.message);
-                // 成功了，自己保存token，跳转到index.html
-                if (res.status === 0) {
-                    // 保存到本地存储中
-                    localStorage.setItem('token', res.token);
-                    location.href = './index.html';
-                }
-            }
-        });
-    });
+   
+  });
 });
